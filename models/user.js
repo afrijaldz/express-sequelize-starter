31a0ed611da9eced20912b6681ac0491915d5e39
Afrijal Dzuhri <ijuldz@gmail.com>
2019-03-25 14:23:15 +0700

    remove password when returning in user model

--- models/user.js
+++ models/user.js
@@ -5,9 +5,18 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     phone: DataTypes.STRING
-  }, {
+  },
+  {
     underscored: true,
   });
+
+  User.prototype.toJSON = function () {
+    let user = Object.assign({}, this.get());
+
+    delete user.password;
+    return user;
+  }
+
   User.associate = function(models) {
     // associations can be defined here
   };
