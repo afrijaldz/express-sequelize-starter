0c6f86e1d2c4b8c8c323055353cdd1d28b244dc5
Afrijal Dzuhri <ijuldz@gmail.com>
2019-03-28 10:56:23 +0700

    fixing error eslint

--- src/controllers/RoleController.js
+++ src/controllers/RoleController.js
@@ -1,59 +1,59 @@
-const RoleService = require('../services/RoleService')
+const RoleService = require('../services/RoleService');
 
 exports.all = async (req, res, next) => {
   try {
-    const roles = await RoleService.getAllRoles()
-    req.data = roles
-    next()
+    const roles = await RoleService.getAll();
+    req.data = roles;
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 exports.store = async (req, res, next) => {
-  const data = req.body
+  const data = req.body;
 
   try {
-    const role = await RoleService.create(data)
-    req.data = role
-    next()
+    const role = await RoleService.create(data);
+    req.data = role;
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 exports.show = async (req, res, next) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   try {
-    const role = await RoleService.getRoleById(id)
-    req.data = role
-    next()
+    const role = await RoleService.getRoleById(id);
+    req.data = role;
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 exports.update = async (req, res, next) => {
-  const { id } = req.params
-  const data = req.body
+  const { id } = req.params;
+  const data = req.body;
 
   try {
-    const role = await RoleService.updateById(id, data)
-    req.data = role
-    next()
+    const role = await RoleService.updateById(id, data);
+    req.data = role;
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 exports.destroy = async (req, res, next) => {
-  const { id } = req.params
+  const { id } = req.params;
   try {
-    const role = await RoleService.removeById(id)
-    req.data = role
-    next()
+    const role = await RoleService.removeById(id);
+    req.data = role;
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
